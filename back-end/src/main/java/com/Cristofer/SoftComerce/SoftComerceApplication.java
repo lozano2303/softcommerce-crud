d1df104a65a0e2486db7e64c6380db7eb270a7ef
Cristofer <cristoferlozano233@gmail.com>
2025-03-08 18:37:23 -0500

    back-end

---
+++ back-end/src/main/java/com/Cristofer/SoftComerce/SoftComerceApplication.java
@@ -0,0 +1,13 @@
+package com.Cristofer.SoftComerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftComerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftComerceApplication.class, args);
+	}
+
+}
